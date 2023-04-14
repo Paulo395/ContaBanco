258e40663a010fcd36d05f86211ac9d56f39fdc1
Paulo Oliveira <paulinho395@gmail.com>
2023-04-14 14:44:52 -0300

    Projeto Exercicio concluido

---
+++ ContaBancaria/Models/Conta.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace ContaBancaria.Models
+{
+    internal class Conta
+    {
+        public int NumConta { get; private set; }
+        public string Nome { get; private set; } //AEEEEEEEEE
+        public double Saldo { get; private set; }
+
+        public Conta(int numConta, string nome)
+        {
+            NumConta = numConta;
+            Nome = nome;
+        }
+
+        public void Depositar(double valor)
+        {
+            Saldo += valor;
+        }
+
+        public void Saque(double valor)
+        {
+            Saldo -= valor;
+            Saldo -= 5;
+        }
+
+        public override string ToString()
+        {
+            return "\nDados da Conta:\nConta: " + NumConta + " , Titular: " + Nome + ", Saldo: $"
+                + Saldo.ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ ContaBancaria/Program.cs
@@ -0,0 +1,42 @@
+﻿using ContaBancaria.Models;
+using System;
+
+namespace ContaBancaria // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            double valor;
+
+            //Console.Write("Bom dia!\nDigite o número da conta: ");
+            int numCon = rnd.Next(0,100);
+
+            Console.Write("Digite o nome do titular da conta: ");
+            string nome = Console.ReadLine();
+
+            Conta c = new Conta(numCon,nome);
+            Console.WriteLine(c);
+
+            Console.WriteLine("Deseja fazer um depósito inicial? (S/N)");
+            string opcao = Console.ReadLine().ToUpper();
+
+            //Duas variaveis não podem ter o mesmo nome, mesmo que sejam de outro escolpo
+            if (opcao.Equals("S"))
+            {
+                Console.Write("Digite o valor do depósito: ");
+                c.Depositar(double.Parse(Console.ReadLine()));
+                Console.WriteLine(c);
+            }
+
+            Console.Write("Entre com um valor para o depósito: ");
+            c.Depositar(double.Parse(Console.ReadLine()));
+            Console.WriteLine(c);
+
+            Console.Write("Entre um valor para o saque: ");
+            c.Saque(double.Parse(Console.ReadLine()));
+            Console.WriteLine(c);
+        }
+    }
+}
